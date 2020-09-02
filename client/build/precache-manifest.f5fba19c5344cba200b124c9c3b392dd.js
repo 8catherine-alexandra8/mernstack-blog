6df35a93eadccadea2450639b543469727f0c0be
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-09-02 09:05:53 -0600

    ran build again

---
+++ client/build/precache-manifest.f5fba19c5344cba200b124c9c3b392dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbe58038dc55ff89f219515e0ca8f7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661e9cec3619ccc0d101",
+    "url": "/static/css/main.39821f9a.chunk.css"
+  },
+  {
+    "revision": "91d67700ceaa95d2ccd8",
+    "url": "/static/js/2.b9b9e93f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b9b9e93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661e9cec3619ccc0d101",
+    "url": "/static/js/main.d93ccd1b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
